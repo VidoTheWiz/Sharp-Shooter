8afb92a4a8451686c7392828b8309737330149ae
VidoTheWiz <mcgreg500@morrisville.edu>
2025-11-17 14:38:43 -0500

    Input Action Shoot configured and OnShoot method added/Enemy health and damage system implemented

--- Sharp Shooter/Assets/Scripts/Weapon.cs
+++ Sharp Shooter/Assets/Scripts/Weapon.cs
@@ -1,15 +1,34 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] int damageAmount = 1;
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
     void Update()
     {
+         HandleShoot();
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Debug.Log(hit.collider.name);
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
+
+            starterAssetsInputs.ShootInput(false);
         }
-        
     }
 }
